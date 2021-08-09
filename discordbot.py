065d205a76ae3947200702d1a6dca0c235c8e0f0
zkmt14 <87798217+zkmt14@users.noreply.github.com>
2021-08-10 00:03:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,6 +12,20 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@bot.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '!hello':
+        await message.channel.send('こんにちは。')
+
+    if message.content == '!z':
+        await message.channel.send('ぜっとだよ！')
+
+    if message.content == '!omikuji':
+        await message.channel.send('あなたの運勢は' + un_omikuji[random.randrange(5)] + '！')
 
 @bot.command()
 async def ping(ctx):
