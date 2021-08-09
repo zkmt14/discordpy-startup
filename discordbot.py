2d2bd5900c94033a63f98f40a22b2e62509df0ae
zkmt14 <87798217+zkmt14@users.noreply.github.com>
2021-08-10 00:09:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,12 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+un_omikuji = ['大吉','中吉','小吉','小凶','中凶','大凶']
 
 @bot.event
 async def on_command_error(ctx, error):
