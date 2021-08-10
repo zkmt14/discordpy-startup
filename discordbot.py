d8ea7c2bc3184afa3bc3597a3f809dc30991e719
zkmt14 <87798217+zkmt14@users.noreply.github.com>
2021-08-10 22:48:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,6 +7,8 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 un_omikuji = ['大吉','中吉','小吉','小凶','中凶','大凶']
+features = ["大きい","小さい","漆黒の","美しい","なんかべとついてる","大量の","おいしそうな","素朴な","豪華な","放置されてる"]
+somethings = ["ハンバーガー","テレビのリモコン","小石","お隣さん","ダイヤモンド","月","とんぼの羽","床の埃","ENTERキー","ピンポン球"]
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -28,6 +30,9 @@ async def on_message(message):
 
     if message.content == '!omikuji':
         await message.channel.send('あなたの運勢は' + un_omikuji[random.randrange(5)] + '！')
+        
+    if message.content == "!item":
+        await message.channel.send("今日のラッキーアイテムは" + features[random.randrange(9)] + somethings[random.randrange(9)] + "！")
 
 @bot.command()
 async def ping(ctx):
