c3be5194e5748db103346bbf56743f1855c30484
zkmt14 <87798217+zkmt14@users.noreply.github.com>
2021-08-27 11:43:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,8 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 import random
-
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 un_omikuji = ['大吉','中吉','小吉','小凶','中凶','大凶']
@@ -15,28 +14,24 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
 @bot.event
 async def on_message(message):
-    
+    # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '!hello':
+        await message.channel.send('こんにちは。')
+    if message.content == '!z':
+        await message.channel.send('ぜっとだよ！')
+
+    if message.content == '!omikuji':
+        await message.channel.send('あなたの運勢は' + un_omikuji[random.randrange(5)] + '！')
+
+    if message.content == "!item":
+        await message.channel.send("今日のラッキーアイテムは" + features[random.randrange(9)] + somethings[random.randrange(9)] + "！")
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
-async def z(ctx):
-    await ctx.send('ぜっとだよ！')
-    
-async def hello(ctx):
-    await ctx.send('こんにちは！')
-    
-async def omikuji(ctx):
-    await ctx.send('あなたの運勢は' + un_omikuji[random.randrange(5)] + '！')
-    
-await def item(ctx):
-    await ctx.send("今日のラッキーアイテムは" + features[random.randrange(9)] + somethings[random.randrange(9)] + "！")
-
-
 bot.run(token)
