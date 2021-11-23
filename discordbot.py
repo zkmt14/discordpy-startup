23029ad6055053a9537a1f29400d266400046766
zkmt14 <87798217+zkmt14@users.noreply.github.com>
2021-11-23 10:58:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,8 +6,8 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 un_omikuji = ['大吉','中吉','小吉','小凶','中凶','大凶']
-features = ["大きい","小さい","漆黒の","美しい","なんかべとついてる","大量の","おいしそうな","素朴な","豪華な","放置されてる"]
-somethings = ["ハンバーガー","テレビのリモコン","小石","お隣さん","ダイヤモンド","月","とんぼの羽","床の埃","ENTERキー","ピンポン球"]
+features = ["大きい","小さい","漆黒の","美しい","なんかべとついてる","大量の","おいしそうな","素朴な","豪華な","放置されてる","カビくさい","青白く光り輝く","妖艶な","超絶怒涛の","アメリカ在住"]
+somethings = ["ハンバーガー","テレビのリモコン","小石","お隣さん","ダイヤモンド","月","とんぼの羽","床の埃","ENTERキー","ピンポン球","カレーライス","ぜっと","カマドウマ","吉野家の牛丼","三ツ矢サイダー"]
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -29,7 +29,7 @@ async def on_message(message):
         await message.channel.send('あなたの運勢は' + un_omikuji[random.randrange(5)] + '！')
 
     if message.content == "!item":
-        await message.channel.send("今日のラッキーアイテムは" + features[random.randrange(9)] + somethings[random.randrange(9)] + "！")
+        await message.channel.send("今日のラッキーアイテムは" + features[random.randrange(15)] + somethings[random.randrange(15)] + "！")
 
 @bot.command()
 async def ping(ctx):
